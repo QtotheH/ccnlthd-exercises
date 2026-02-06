d333c84be1b536852ba711112b17a697df4b6906
Nhun <lethihongnhungntt@gmail.com>
2026-02-06 21:50:38 +0700

    Hoàn thành bt chapter 6

---
+++ Chapter 06/React-Learn/my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import AdvancedRegisterForm from "./AdvancedRegisterForm";
+
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <AdvancedRegisterForm />
+    </div>
+  );
+}
+
+export default App;
